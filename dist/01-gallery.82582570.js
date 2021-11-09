c5c73bab22cfa52b855a4fb24622134df13cfdcf
VIKTORIAIT <89775075+VIKTORIAIT@users.noreply.github.com>
2021-11-09 16:29:08 +0200

    changes

--- dist/01-gallery.82582570.js
+++ dist/01-gallery.82582570.js
@@ -124,7 +124,7 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.galleryItems = void 0;
-var galleryItems = [{
+const galleryItems = [{
   preview: 'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
   original: 'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
   description: 'Hokkaido Flower'
@@ -198,7 +198,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53821" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59641" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
