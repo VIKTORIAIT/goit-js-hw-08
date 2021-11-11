4ccc5b833d86a05672442f2b6e649534d5106cea
VIKTORIAIT <89775075+VIKTORIAIT@users.noreply.github.com>
2021-11-11 16:33:46 +0200

    Made 2, 3 tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+let localItem = localStorage.getItem('videoplayer-current-time');
+player.setCurrentTime(localItem).then(function (seconds) {});
+
+player.on(
+  'timeupdate',
+  throttle(function func(ev) {
+    localStorage.setItem('videoplayer-current-time', `${ev.seconds}`);
+  }, 1000),
+);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+const throttle = require('lodash.throttle');
 
+const form = document.querySelector('.feedback-form');
+
+function updateOutput() {
+  const dataLocal = localStorage.getItem('feedback-form-state');
+  const dataLocalObj = JSON.parse(dataLocal);
+  if (!dataLocal) {
+    return;
+  }
+  form.elements.email.value = dataLocalObj.email || '';
+  form.elements.message.value = dataLocalObj.message || '';
+}
+
+updateOutput();
+form.addEventListener(
+  'input',
+  throttle(ev => {
+    const storage = {
+      email: form.elements.email.value,
+      message: form.elements.message.value,
+    };
+    const storageStr = JSON.stringify(storage);
+    localStorage.setItem('feedback-form-state', storageStr);
+    updateOutput();
+  }, 500),
+);
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  console.log({ email: form.elements.email.value, message: form.elements.message.value });
+  e.currentTarget.reset();
+  localStorage.clear();
+});
